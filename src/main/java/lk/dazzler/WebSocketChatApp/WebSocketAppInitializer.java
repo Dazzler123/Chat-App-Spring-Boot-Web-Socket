25302bf2a4ffd70afa4b4e6162fdf54165536fee
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-11 14:59:13 +0530

    Content updated

--- src/main/java/lk/dazzler/WebSocketChatApp/WebSocketAppInitializer.java
+++ src/main/java/lk/dazzler/WebSocketChatApp/WebSocketAppInitializer.java
@@ -1,5 +1,4 @@
 package lk.dazzler.WebSocketChatApp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class WebSocketAppInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketAppInitializer.class, args);
 	}
-
 }
